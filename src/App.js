f330a24388d8d65000e6669a59ae0ae2b5e6a18b
Brandon Temba <brandontemba560@gmail.com>
2022-07-31 23:14:17 +0300

    Step1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
+import React, { useState } from "react";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
- 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  return <div>Hello Temba</div>;
+};
 
 export default App;
