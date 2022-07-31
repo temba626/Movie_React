fea95b57a7acbe3494202df6e82819d6241c43fa
Brandon Temba <brandontemba560@gmail.com>
2022-07-31 23:40:08 +0300

    Step 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?i=tt3896198&apikey=ac5d2b5"
+
+    const response = await fetch(url);
+
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+  }
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+
   return (
-  <div className="container-fluid">
+  <div className='container-fluid movie-app'>
     <div className="row">
       <MovieList movies={movies} />
     </div>  
